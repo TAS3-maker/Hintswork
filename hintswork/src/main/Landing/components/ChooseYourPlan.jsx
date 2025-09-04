b0073d118a78f337e78c045de9b5dc62423dfc49
TAS3-maker <dm@techarchsoftwares.com>
2025-09-04 12:31:06 +0530

    Update ChooseYourPlan.jsx

--- hintswork/src/main/Landing/components/ChooseYourPlan.jsx
+++ hintswork/src/main/Landing/components/ChooseYourPlan.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import groupPlan from "../assets/group2.png"
+import React from 'react';
+import groupPlan from "../assets/group2.png";
+
 const ChooseYourPlan = () => {
   return (
-    <div className='mt-20'>
-          <h2 className="text-5xl md:text-6xl font-bold text-center ml-160 md:text-left ">
+    <div className="mt-20 px-6 lg:px-70 md:px-0  max-w-7xl mx-auto">
+      <h2 className="text-4xl md:text-6xl font-bold text-center md:ml-30 md:text-left">
         Choose <span className="text-red-500">Your Plan</span>
       </h2>
-      <p className='text-gray-500 text-2xl ml-163 mt-5 '>Start Free. Upgrade anytime. No commitments.</p>
-      <img src={groupPlan} alt="" className='ml-100 mt-15' />
+      <p className="text-gray-500 text-lg md:text-1xl text-center md:text-left mt-4 max-w-xl md:ml-34 mx-auto md:mx-4">
+        Start Free. Upgrade anytime. No commitments.
+      </p>
+      <div className="mt-8 flex justify-center md:ml-10 md:justify-start">
+        <img
+          src={groupPlan}
+          alt="Group Plan"
+          className="w-full max-w-md md:max-h-100 md:max-w-3xl"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChooseYourPlan
+export default ChooseYourPlan;
