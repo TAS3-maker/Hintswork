40db6ef2c5956ca8b721f6b7e09c161872d70f62
TAS3-maker <dm@techarchsoftwares.com>
2025-09-04 12:33:12 +0530

    Update HIW.jsx

--- hintswork/src/main/Landing/components/HIW.jsx
+++ hintswork/src/main/Landing/components/HIW.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import HowItWorks from '../assets/howitworks.png'
+import HowItWorks from '../assets/HowItWorks.png'
+import { Element } from 'react-scroll'
 const HIW = () => {
   return (
-    <div className='relative'>
-        <img src={HowItWorks} alt="HowItWorks" className='ml-30' />
-        <button className="bg-red-500 text-white  absolute px-7 py-3 rounded-md  mb-10 ml-130 text-lg font-semibold shadow-md">
+    <Element name='HIW'>
+    <div className='relative mt-20 lg:mt-20'>
+        <img src={HowItWorks} alt="HowItWorks" className='md:w-[100%] overflow-x-hidden lg:w-[90%] lg:ml-20' />
+        <button className="bg-red-500 text-white absolute px-7 py-3 rounded-md  mb-10 ml-[90px]  md:ml-70 lg:ml-130 text-lg font-semibold shadow-md">
 Try a Sample Hint      
 </button>
 
 
     </div>
+    </Element>
   
 
   )
