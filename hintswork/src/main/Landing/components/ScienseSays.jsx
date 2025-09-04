b701296516a4f398765a27447cb2128902979166
TAS3-maker <dm@techarchsoftwares.com>
2025-09-04 12:34:03 +0530

    Update ScienseSays.jsx

--- hintswork/src/main/Landing/components/ScienseSays.jsx
+++ hintswork/src/main/Landing/components/ScienseSays.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import messageIcon from '../assets/messageIcon.png'; 
 import scienceSays from '../assets/scienseSays.png';
+import { Element } from 'react-scroll';
 const ScienceSays = () => {
   return (
-    <div className="py-8 px-4 md:px-12 items-center mt-30 justify-center">
-      {/* Section Heading */}
-      <h2 className="text-5xl md:text-5xl font-bold text-center ml-160 md:text-left mb-6">
+    <Element name='Science'>
+
+    <div className="py-8 px-6 max-w-7xl mx-auto">
+      <h2 className="text-4xl md:text-6xl  font-bold text-center md:text-center mb-8">
         Science <span className="text-red-500">Says...</span>
       </h2>
 
-      {/* Content Flex Container */}
-      <div className="flex flex-col mt-20 md:flex-row items-center md:items-start gap-6 ml-25">
-        {/* Left Image Column */}
-        <div className="flex flex-col items-center md:items-start space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
-          {/* Message Icon */}
+      <div className="flex flex-col  md:flex-row items-center lg:ml-45  md:ml-7 md:items-center gap-8">
+        <div className="flex flex-row items-center space-x-6 mb-8 md:mb-0">
           <img
             src={messageIcon}
             alt="Message Icon"
-            className="w-20 h-20 mt-60 mr-30"
+            className="w-16 h-16 md:w-14 md:h-10 md:mt-60 object-contain"
           />
 
-          {/* Middle Group Picture */}
           <img
             src={scienceSays}
             alt="Group Picture"
-            className="w-90 h-90 rounded-lg object-cover mr-5"
+            className="w-40 h-40 md:w-40 md:h-40   rounded-lg object-cover"
           />
         </div>
 
-        {/* Text Column */}
-        <div className="text-center md:text-left ml-20 max-w-md w-full">
-          <h3 className="text-4xl font-bold mb-2">Science Says</h3>
-          <p className="text-gray-500 text-2xl ">
+        <div className="text-center md:text-left md:mt-15 max-w-md">
+          <h3 className="text-3xl md:text-3xl font-bold mb-4">Science Says</h3>
+          <p className="text-gray-500 text-lg md:text-md leading-relaxed">
             Hints Work! is grounded in behavioral science. Studies indicate that
             small, consistent nudges can improve long-term outcomes sustainably.
             Each Hint you see is connected to research and real-world evidence in
@@ -40,6 +37,7 @@ const ScienceSays = () => {
         </div>
       </div>
     </div>
+    </Element>
   );
 };
 
